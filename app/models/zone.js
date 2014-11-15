a22c00097bee14b97e260f578c3ca91321131f71
Garrett Heinlen <heinleng@gmail.com>
2014-11-15 22:58:48 +1100

    Adding in zones resource.

---
+++ app/models/zone.js
@@ -0,0 +1,10 @@
+import DS from "ember-data";
+
+export default DS.Model.extend({
+  parent: DS.belongsTo('zone', { async: true, inverse: 'zones' }),
+  zones: DS.hasMany('zone', {async: true, inverse: 'parent'}),
+
+  title: DS.attr('string'),
+  desc: DS.attr('string'),
+  root: DS.attr('boolean'),
+});
