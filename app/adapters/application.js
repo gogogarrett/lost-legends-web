fbdd7a1b279b17e2020edbd509c4a561bffb688c
Garrett Heinlen <heinleng@gmail.com>
2014-11-19 14:57:01 +1100

    making ember rest adapter more rails-like

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,7 +1,16 @@
 import Ember from "ember";
 import DS from "ember-data";
 
+var decamelize = Ember.String.decamelize,
+    underscore = Ember.String.underscore;
+
 export default DS.RESTAdapter.extend({
   namespace: 'api/v1',
-  coalesceFindRequests: true
+  coalesceFindRequests: true,
+
+  pathForType: function (type) {
+    var decamelized = decamelize(type);
+    var underscored = underscore(decamelized);
+    return Ember.String.pluralize(underscored);
+  },
 });
