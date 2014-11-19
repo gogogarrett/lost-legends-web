6daa97eceeb7fdbe1eb49138eaac83df3bdf8380
Garrett Heinlen <heinleng@gmail.com>
2014-11-19 14:58:44 +1100

    Hooking up the battle route to save the model / update the template

--- app/routes/zone/battle.js
+++ app/routes/zone/battle.js
@@ -15,51 +15,87 @@ export default Ember.Route.extend({
 
   model: function(params) {
     var zoneId = this.modelFor('zone').get('id');
-    return this.store.find('battle', zoneId);
+    var player = this.get('session.player');
+    var store = this.store;
+
+    var idCreateHash = {
+      player_id: player.get('id'),
+      battle_id: zoneId,
+    };
+
+    return Em.$.post('/api/v1/player_battles', { player_battle: idCreateHash }).then(function(data) {
+      var battleId = data.player_battle.id;
+      return store.find('playerBattle', battleId);
+    });
   },
 
   afterModel: function (model, _) {
-    var player = this.get('session.player');
-
-    // update players exp
-    if ( !isNaN(model.get('exp')) ) {
-      var newExp = player.get('exp') + model.get('exp');
-      player.set('exp', newExp);
-    }
-
-    // update players rubies
-    if ( !isNaN(model.get('rubies')) ) {
-      var newRubies = player.get('rubies') + model.get('rubies');
-      player.set('rubies', newRubies);
-    }
-
-    // create inventory objects for the join between player / item
-    var store = this.store;
-    model.get('items').then(function(items) {
-      items.forEach(function(item) {
-        var itemType = item.get('type');
-
-        var idCreateHash = {
-          player_id: player.get('id'),
-          item_id: item.get('id'),
-          item_type: itemType,
-        };
-
-        // Normal #save doesn't send IDS..lame
-        Em.$.post('/api/v1/inventories', { inventory: idCreateHash }).then(function(inventory) {
-          console.log(inventory);
-        })
-      })
-    })
-
-    player.save();
     return model.get('zone');
   },
 
-  resetController: function (controller, isExiting, transition) {
-    // remove all the battle references in the store
-    // forcing to go back to the server to get the random'ish gameplay
-    if (isExiting) this.store.unloadAll('battle');
-  }
+  setupController: function(controller, model) {
+    // this.super();
+    controller.set('model', model);
 
+    var zoneId = this.modelFor('zone').get('id');
+    controller.set('zoneId', zoneId);
+  },
+
+  actions: {
+    attack: function() {
+      // [g] why do I need context..instead of model?
+      var context = this.get('context');
+      var player = this.get('session.player');
+      var store = this.store;
+
+      // [g] gross
+      // if (Math.floor(Math.random() * 100) > 50) {
+        context.set('status', 'won');
+      // } else {
+        // context.set('status', 'fail');
+      // }
+
+      context.save().then(function(model) {
+
+        if ( model.get('complete') ) {
+
+          if ( model.get('won') ) {
+
+            // update players exp
+            if ( !isNaN(model.get('exp')) ) {
+              var newExp = player.get('exp') + model.get('exp');
+              player.set('exp', newExp);
+            }
+
+            // update players rubies
+            if ( !isNaN(model.get('rubies')) ) {
+              var newRubies = player.get('rubies') + model.get('rubies');
+              player.set('rubies', newRubies);
+            }
+
+            model.get('items').then(function(items) {
+              items.forEach(function(item) {
+                var itemType = item.get('type');
+
+                var idCreateHash = {
+                  player_id: player.get('id'),
+                  item_id: item.get('id'),
+                  item_type: itemType,
+                };
+
+                // Normal #save doesn't send IDS..lame
+                Em.$.post('/api/v1/inventories', { inventory: idCreateHash }).then(function(inventory) {
+                  console.log(inventory);
+                })
+              })
+            })
+
+            player.save().then(function(player) {
+              console.log(player);
+            });
+          }
+        }
+      });
+    }
+  }
 });
