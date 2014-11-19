a5f5efde89283967041a6e32c7c1019f6871503a
Garrett Heinlen <heinleng@gmail.com>
2014-11-19 14:57:23 +1100

    Hooking up the battle -> player battle relationship

--- app/models/battle.js
+++ app/models/battle.js
@@ -4,6 +4,8 @@ export default DS.Model.extend({
   zone: DS.belongsTo('zone', { async: true }),
   items: DS.hasMany('items', { async: true }),
 
+  player_battles: DS.hasMany('player_battle', { async: true }),
+
   win: DS.attr('boolean'),
   exp: DS.attr('number'),
   rubies: DS.attr('number'),
---
+++ app/models/player-battle.js
@@ -0,0 +1,20 @@
+import DS from "ember-data";
+
+export default DS.Model.extend({
+  zone: DS.belongsTo('zone', { async: true }),
+
+  player: DS.belongsTo('player', { async: true }),
+  battle: DS.belongsTo('battle', { async: true }),
+  monster: DS.belongsTo('monster', { async: true }),
+  items: DS.hasMany('items', { async: true }),
+
+  inProgress: Em.computed.equal('status', 'in-progress'),
+  complete: Em.computed.not('inProgress'),
+
+  won: Em.computed.equal('status', 'won'),
+  fail: Em.computed.equal('status', 'fail'),
+
+  status: DS.attr('string'),
+  rubies: DS.attr('number'),
+  exp: DS.attr('number'),
+});
