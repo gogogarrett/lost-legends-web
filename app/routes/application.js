8ca5981cd3721266c0d23afa6f2c6d99721a8a78
Garrett Heinlen <heinleng@gmail.com>
2014-11-21 12:06:37 +1100

    creating a heal button.

--- app/routes/application.js
+++ app/routes/application.js
@@ -3,5 +3,14 @@ import Ember from "ember";
 export default Ember.Route.extend({
   beforeModel: function() {
     this.get('session').set('user_id', 1)
+  },
+
+  actions: {
+    heal: function() {
+      var player = this.get('session.player');
+      var maxHealth = player.get('max_health');
+      player.set('current_health', maxHealth);
+      player.save();
+    }
   }
 });
